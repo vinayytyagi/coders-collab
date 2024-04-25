4af7ab539b00826649c6112a7614b22bc7e85989
Vinay Kumar <tyagivinay807@gmail.com>
2024-04-26 02:20:26 +0530

    finish setting up next-auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider"
 import { ModeToggle } from "@/components/mode-toggle";
+import { Providers } from "./provider";
+import { Header } from "./header";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,18 +21,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <div>
-              <ModeToggle />
-            </div>
-            {children}
-          </ThemeProvider>
-        </body>
+        <Providers>
+          {" "}
+          <Header />
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
